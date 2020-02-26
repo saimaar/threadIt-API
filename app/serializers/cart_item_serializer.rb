76e2934ec13bfcc2667a81dae733c1730fa0fe20
saimaar <rahman.saima92@gmail.com>
2020-02-26 11:15:41 -0500

    added some comments

--- app/serializers/cart_item_serializer.rb
+++ app/serializers/cart_item_serializer.rb
@@ -1,3 +1,8 @@
 class CartItemSerializer < ActiveModel::Serializer
   attributes :id, :cart_id, :item_id
 end
+
+# here belongs_to would make all the difference
+# attributes :id, :item
+# belongs_to :cart
+# belongs_to :item
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,25 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email, :orders, :cart_items
-  has_one :cart
+  # has_one :cart
 
   def cart_items
     # byebug
       self.object.cart.cart_items.map do |cart_item|
-
         cart_item.item.to_h.merge({:cart_item_id => cart_item.id})
     end
   end
 end
+
+
+
+
+# 2nd way of having cart_item id inside each item instance
+#Also check cart_item Serializer
+# attributes :id, :username, :email, :orders
+# has_one :cart
+# def cart
+#   # byebug
+#     self.object.cart.cart_items.map do |cart_item|
+#       cart_item
+#   end
+# end
